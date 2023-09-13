3d82741628156ee5ff6be2b2e619c811f8395972
ronik-v <andronnikovvv@gmail.com>
2023-09-13 16:59:13 +0300

    user interface

---
+++ src/main/java/com/ronik/blog/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ronik.blog.repo;
+
+
+import com.ronik.blog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
